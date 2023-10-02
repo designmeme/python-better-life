9b70ce74290ca46f008826aad4c2d9fa16b3226e
jihye <ghe.lee19@gmail.com>
2023-10-02 11:07:07 +0900

    feat(book): 메세지로 보낸 도서 다시 메세지 보내지 않기

--- better_life/book.py
+++ better_life/book.py
@@ -17,6 +17,7 @@ dotenv.load_dotenv()
 def notify_new_book_by_keyword(keyword: str):
     """신간 알림
     키워드로 검색한 결과에 신간 도서가 있다면 텔레그램 메세지로 알려준다.
+    메세지로 보낸 도서는 .cache/book.csv 파일에 저장되며 다음 알림에서 제외된다.
 
     신간 기준: 검색 실행일 기준 하루 전에 발행한 경우
 
@@ -43,6 +44,7 @@ def notify_new_book_by_keyword(keyword: str):
 
         books = res.json()["items"]
         books = pd.DataFrame(books)
+        books = books.set_index("isbn")
         books['pubdate'] = pd.to_datetime(books['pubdate'])
         logger.debug(f"검색 결과: {len(books)}개.\n{books[['pubdate', 'title']]}")
 
@@ -51,13 +53,28 @@ def notify_new_book_by_keyword(keyword: str):
         # 어제 이후 출간된 도서만
         new_books = books[books['pubdate'] >= today - datetime.timedelta(days=1)]
 
+        # 이미 발송한 도서
+        if not os.path.isdir("../.cache"):
+            os.makedirs("../.cache")
+        try:
+            old_books = pd.read_csv("../.cache/books.csv", index_col="isbn", dtype={"isbn": str},
+                                    parse_dates=["pubdate"])
+            # 이미 발송한 도서 제외
+            new_books = new_books[~new_books.index.isin(old_books.index.tolist())]
+        except FileNotFoundError:
+            old_books = None
+
         # 신간 도서가 있다면 메세지를 발송한다.
         logger.debug(f"신간 검색 결과: {len(new_books)}개")
 
         if len(new_books) > 0:
             text = [f"*신간 알림* - 검색어 \"{keyword}\""]
             for i, b in new_books.iterrows():
-                text.append(f"{i + 1}. {b['pubdate'].strftime('%Y-%m-%d')} [{b['title']}]({b['link']})")
+                text.append(f"* {b['pubdate'].strftime('%Y-%m-%d')} [{b['title']}]({b['link']})")
+
+            # 메세지 발송 책을 캐시 파일로 저장한다. (description 내용이 길어서 삭제함)
+            cache_books = pd.concat([new_books, old_books]) if old_books is not None else new_books
+            cache_books.drop("description", axis=1).to_csv("../.cache/books.csv", date_format="%Y-%m-%d")
         else:
             text = [f"*신간 알림* - 검색어 \"{keyword}\""]
             text += [f"- 없음"]
