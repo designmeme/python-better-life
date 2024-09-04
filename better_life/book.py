8b40fb819a87bfad7200ccef7eb14c3f8435d448
jihye <ghe.lee19@gmail.com>
2024-09-05 00:08:05 +0900

    fix(book): 신간 알림 오류 수정
    1. isbn 없는 경우 반복 메세지 보내는 것 수정
    2. 메세지 마크업 오류 수정
    3. 발송 내역 관리 개선

--- better_life/book.py
+++ better_life/book.py
@@ -14,9 +14,6 @@ logger = logging.getLogger(__name__)
 dotenv.load_dotenv()
 
 
-_pattern = {i: '\\' + chr(i) for i in b'()[]{}?*+-|^$\\.&~#'}
-
-
 def notify_new_book_by_keyword(keyword: str, file: str):
     """신간 알림
     키워드로 검색한 결과에 신간 도서가 있다면 텔레그램 메세지로 알려준다.
@@ -28,6 +25,7 @@ def notify_new_book_by_keyword(keyword: str, file: str):
     https://developers.naver.com/docs/serviceapi/search/book/book.md
 
     :param keyword: 검색어
+    :param file: 발송 내역 저장 파일
     :return:
     """
     try:
@@ -48,50 +46,53 @@ def notify_new_book_by_keyword(keyword: str, file: str):
         books = res.json()["items"]
         books = pd.DataFrame(books)
         books = books.set_index("isbn")
+        books = books.loc[books.index != '']  # 세트 도서는 ISBN 정보가 없음. 제외 처리함.
+        books = books[['pubdate', 'title', 'link']]
+
         books['pubdate'] = pd.to_datetime(books['pubdate'])
+        books = books.sort_values(['pubdate', 'isbn'], ascending=[False, True])
+
         logger.debug(f"검색 결과: {len(books)}개.\n{books[['pubdate', 'title']]}")
 
-        today = datetime.datetime.fromisoformat(datetime.date.today().isoformat())
-
-        # 어제 이후 출간된 도서만
-        new_books = books[books['pubdate'] >= today - datetime.timedelta(days=1)]
-
-        # 이미 발송한 도서
         directory = os.path.dirname(os.path.abspath(file))
         if not os.path.isdir(directory):
             os.makedirs(directory)
+
+        # 이미 발송한 도서 정보를 가져온다.
         try:
             old_books = pd.read_csv(file, index_col="isbn", dtype={"isbn": str},
                                     parse_dates=["pubdate"])
             # 이미 발송한 도서 제외
-            new_books = new_books[~new_books.index.isin(old_books.index.tolist())]
+            new_books = books[~books.index.isin(old_books.index.tolist())]
         except FileNotFoundError:
+            # 최초 실행시 첫 도서만 포함.
+            new_books = books.iloc[:1]
             old_books = None
 
         # 신간 도서가 있다면 메세지를 발송한다.
         logger.debug(f"신간 검색 결과: {len(new_books)}개")
 
-        if len(new_books) > 0:
-            text = [f"*신간 알림* - 검색어 \"{keyword}\""]
+        if len(new_books):
+            text = [f"<b>신간 알림</b> - 검색어 '{keyword}'"]
             for i, (isbn, b) in enumerate(new_books.iterrows()):
-                text.append(f"{i + 1}. {b['pubdate'].strftime('%Y-%m-%d')} [{b['title'].translate(_pattern)}]({b['link']})")
+                text.append(
+                    f"{i + 1}. {b['pubdate'].strftime('%Y-%m-%d')}"
+                    f' <a href="{b["link"]}">{b["title"]}</a>'
+                )
 
-            # 메세지 발송 책을 캐시 파일로 저장한다. (description 내용이 길어서 삭제함)
+            # 메세지 발송 책을 캐시 파일로 저장한다.
             cache_books = pd.concat([new_books, old_books]) if old_books is not None else new_books
-            cache_books.drop("description", axis=1).to_csv(file, date_format="%Y-%m-%d")
-        else:
-            pass
-            # text = [f"*신간 알림* - 검색어 \"{keyword}\""]
-            # text += [f"- 없음"]
+            cache_books = cache_books.sort_values('pubdate', ascending=False)
+            cache_books = cache_books.head(50)  # 최신 데이터만 저장한다.
+            cache_books.to_csv(file, date_format="%Y-%m-%d")
 
-        text = "\n".join(text)
-        # logger.debug(text)
-        asyncio.run(telegrambot.send_message(text, "Markdown"))
+            text = "\n".join(text)
+            asyncio.run(telegrambot.send_message(text))
 
     except Exception as e:
-        logger.error(traceback.format_exception())
-        text = f"신간 알림 코드 실행 중 오류가 발생했어요. {e}"
-        asyncio.run(telegrambot.send_message(text, "Markdown"))
+        logger.error(traceback.format_exc())
+        text = f"신간 알림 코드 실행 중 오류가 발생했어요.\n{e}"
+        asyncio.run(telegrambot.send_message(text))
 
 
 if __name__ == "__main__":
